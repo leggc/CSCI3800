6ad02e63ec3b0ad764d9384ac716929500a03716
leggc <leggc@users.noreply.github.com>
2016-05-04 14:29:39 -0600

    Added files via upload

---
+++ Project/App/app.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+// Declares how the application should be bootstrapped. See: http://docs.angularjs.org/guide/module
+angular.module('app',
+    [
+        // Dependency Inject Goes inside this array
+        'ui.router',  // we inject/load ui-router for routing
+        'app.controllers', // we inject/load the controllers
+    ]
+)
+    .config(['$stateProvider',
+        function ($stateProvider) {
+            // UI States, URL Routing & Mapping. For more info see: https://github.com/angular-ui/ui-router
+
+            // our routers, self explanatory
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: './View/Home.html',
+                })
+                .state('movies', {
+                    url: '/movies',
+                    templateUrl: './View/Movies.html',
+                    controller: 'MoviesController'
+                })
+                .state('otherwise', {
+                    url: '*path',
+                    templateUrl: '/View/Error.html',
+                    controller: 'ErrorCtrl'
+                });
+        }]
+
+        );
---
+++ Project/App/controllers.js
@@ -0,0 +1,16 @@
+﻿angular.module('app.controllers', [])
+
+    .controller('MoviesController', function ($scope, $http)
+    {
+
+        $http({
+            method: 'GET',
+            url: 'https://api.usergrid.com/leggc/sandbox/movies'
+        })
+         .success(function (data)
+         {             
+             $scope.movies = data;
+         });
+        
+
+    });
